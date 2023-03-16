f2028b5d69611efaa2b944161eda83fce5c31b01
luminaries1 <luminaries1@naver.com>
2023-03-16 10:20:21 +0900

    view print 구문 정리
    Former-commit-id: b34f38feca0020a414a2abb428bbacd3fcafb31c

--- djago_ai/django_ai/aimodule/views.py
+++ djago_ai/django_ai/aimodule/views.py
@@ -20,7 +20,7 @@ kor_eng_dic_hard_coding = {"stairs":"계단","pig":"돼지","octopus":"문어","
 "streetlight":"가로등","potato":"감자","apple":"사과","camera":"카메라","crab":"게","eyeglasses":"안경","ant":"개미","bed":"침대","boomerang":"부메랑","computer":"컴퓨터","couch":"소파","ear":"귀","helicopter":"헬리콥터","keyboard":"키보드","purse":"지갑","tornado":"토네이도","nail":"손톱","pillow":"베개","radio":"라디오","scorpion":"전갈","snowflake":"눈송이",
 "tiger":"호랑이","rabbit":"토끼","backpack":"백팩","car":"자동차","crocodile":"악어","fish":"물고기","arm":"팔","harp":"하프","bracelet":"팔찌","castle":"성","diamond":"다이아몬드","elbow":"팔꿈치","foot":"발","knee":"무릎","sink":"싱크대","moon":"달","necklace":"목걸이","pizza":"피자","rain":"비","screwdriver":"드라이버","soccer ball":"축구공",
 "toothbrush":"칫솔","raccoon":"너구리","banana":"바나나","carrot":"당근","cup":"컵","flower":"꽃","axe":"도끼","belt":"혁대","brain":"뇌","cell phone":"휴대폰","dishwasher":"식기세척기","envelope":"봉투","fork":"포크","ladder":"사다리","skull":"해골","mosquito":"모기","nose":"코","pond":"연못","rainbow":"무지개","wine glass":"와인잔","snowman":"눈사람",
-"toothpaste":"치약","river":"강","bee":"벌","cat":"고양이","duck":"오리","frog":"개구리","bandage":"붕대","bread":"빵","cactus":"선인장","cello":"첼로","dog":"개","lamp":"램프","hot dog":"핫도그","hamburger":"햄버거","skyscraper":"빌딩","train":"기차","table":"테이블","parachute":"낙하산","remote control":"리모콘","sheep":"양","sock":"양말",
+"toothpaste":"치약","river":"강","bee":"벌","cat":"고양이","duck":"오리","frog":"개구리","bandage":"붕대","bread":"빵","cactus":"선인장","cello":"첼로","dog":"개","floor lamp":"램프","hot dog":"핫도그","hamburger":"햄버거","skyscraper":"빌딩","train":"기차","table":"테이블","parachute":"낙하산","remote control":"리모콘","sheep":"양","sock":"양말",
 "traffic light":"신호등","shark":"상어","bicycle":"자전거","chair":"의자","elephant":"코끼리","giraffe":"기린","baseball bat":"야구방망이","broccoli":"브로콜리","cake":"케이크","church":"교회","dolphin":"돌고래","face":"얼굴","house":"집","leaf":"나뭇입","fence":"울타리","tree":"나무","octagon":"팔각형","passport":"여권","paper clip":"클립","zebra":"얼룩말","spider":"거미",
 "watermelon":"수박","shoe":"신발","hospital":"병원","kangaroo":"캥거루","horse":"말","grapes":"포도","bat":"박쥐","bucket":"양동이","calculator":"계산기","circle":"원","donut":"도넛","feather":"깃털","ice cream":"아이스크림","leg":"다리","finger":"손가락","triangle":"삼각형","sword":"칼","peanut":"땅콩","panda":"판다","star":"별","spoon":"숟가락",
 "whale":"고래","snail":"달팽이","key":"열쇠","laptop":"노트북","lion":"사자","microphone":"마이크","bathtub":"욕조","bird":"새","calendar":"달력","candle":"양초","door":"문","drill":"드릴","jacket":"재킷","light bulb":"전구","grass":"잔디","umbrella":"우산","t-shirt":"티셔츠","rhinoceros":"코뿔소","roller coaster":"롤러코스터","steak":"스테이크","sun":"태양",
@@ -45,16 +45,15 @@ def make_img(img_arr) :
     return image 
 
 
-class_label = ['airplane', 'alarm_clock', 'ambulance', 'angel', 'ant', 'anvil', 'apple', 'arm', 'asparagus', 'axe', 'backpack']
+class_label = ['airplane', 'angel', 'ant', 'apple', 'arm', 'axe', 'backpack', 'banana', 'bandage', 'baseball bat', 'bat', 'bathtub', 'beach', 'bear', 'beard', 'bed', 'bee', 'belt', 'bicycle', 'bird', 'blueberry', 'book', 'boomerang', 'bracelet', 'brain', 'bread', 'broccoli', 'broom', 'bucket', 'bus', 'bush', 'butterfly', 'cactus', 'cake', 'calculator', 'calendar', 'camel', 'camera', 'candle', 'cannon', 'canoe', 'car', 'carrot', 'castle', 'cat', 'cell phone', 'cello', 'chair', 'church', 'circle', 'clock', 'cloud', 'compass', 'computer', 'cookie', 'couch', 'cow', 'crab', 'crayon', 'crocodile', 'crown', 'cup', 'diamond', 'dishwasher', 'dog', 'dolphin', 'donut', 'door', 'dragon', 'drill', 'duck', 'ear', 'elbow', 'elephant', 'envelope', 'eraser', 'eyeglasses', 'face', 'feather', 'fence', 'finger', 'firetruck', 'fish', 'floor lamp', 'flower', 'foot', 'fork', 'frog', 'giraffe', 'grapes', 'grass', 'guitar', 'hamburger', 'hammer', 'hand', 'harp', 'hat', 'headphones', 'helicopter', 'horse', 'hospital', 'hot dog', 'house', 'ice cream', 'jacket', 'jail', 'kangaroo', 'key', 'keyboard', 'knee', 'ladder', 'laptop', 'leaf', 'leg', 'light bulb', 'lightning', 'lion', 'lipstick', 'map', 'matches', 'microphone', 'monkey', 'moon', 'mosquito', 'mountain', 'mouse', 'mouth', 'mushroom', 'nail', 'necklace', 'nose', 'ocean', 'octagon', 'octopus', 'onion', 'oven', 'owl', 'paintbrush', 'panda', 'pants', 'paper clip', 'parachute', 'passport', 'peanut', 'pencil', 'penguin', 'piano', 'picture frame', 'pig', 'pillow', 'pineapple', 'pizza', 'pond', 'potato', 'purse', 'rabbit', 'raccoon', 'radio', 'rain', 'rainbow', 'remote control', 'rhinoceros', 'river', 'roller coaster', 'sandwich', 'saw', 'scissors', 'scorpion', 'screwdriver', 'shark', 'sheep', 'shoe', 'sink', 'skull', 'skyscraper', 'snail', 'snake', 'snowflake', 'snowman', 'soccer ball', 'sock', 'spider', 'spoon', 'stairs', 'star', 'steak', 'strawberry', 'streetlight', 'submarine', 'sun', 'swan', 'sword', 't-shirt', 'table', 'tiger', 'tooth', 'toothbrush', 'toothpaste', 'tornado', 'traffic light', 'train', 'tree', 'triangle', 'umbrella', 'watermelon', 'whale', 'wheel', 'windmill', 'wine glass', 'zebra']
+# class_label = ['airplane', 'alarm clock', 'ambulance', 'angel', 'ant', 'anvil', 'apple', 'arm', 'asparagus', 'axe', 'backpack']
+
 
 @api_view(['POST'])
 def ai_post(request):
     draw = request.data['drawing']
     width = request.data['canvasWidth']
     height = request.data['canvasHeight']
-    widht = int(width)
-    height = int(height)
-    print(draw)
     # for문 3번 쓰는거 numpy 활용해서 바꿀 생각 하기.
     for i in range(len(draw)):
         for j in range(len(draw[i])):
@@ -65,12 +64,29 @@ def ai_post(request):
                     draw[i][j][k] = int(draw[i][j][k]*256//height)
                     
     img = make_img(draw)
+
+    # plot 을 위해
     # plt.imshow(img)
     # plt.show()
+
     img = np.array(img.resize((64,64))) 
     img = img.reshape(64,64,1)
-    # print(model.predict(np.array([img])))
+
+    # 콘솔에 print 좀 해달라고 해서 
+
+    # print_array = (model.predict(np.array([img]))*100)
+    # print(class_label)
+    # print_array =np.round(print_array, 1)[0]
+    # print_array = list(map(str, print_array))
+    # for i in range(len(print_array)):
+    #     print_array[i] += '%'
+    # print(print_array)
     # print(class_label[np.argmax(model.predict(np.array([img])))])
+    # print('Response :' ,{'predictionType' : 0, 'firstPrediction' : prediction_first_label , 'secondPrediction' : ''} )
+    # print('Response :' ,{'predictionType' : 0, 'firstPrediction' : prediction_first_label , 'secondPrediction' : prediction_second_label} )
+    # print('Response :' ,{'predictionType' : 2, 'firstPrediction' : '' , 'secondPrediction' : ''} )
+
+
     prediction_arr = model.predict(np.array([img]))[0]
     prediction_first_index = np.argmax(prediction_arr)
     prediction_second_index = np.argsort(prediction_arr, axis=0 )[-2]
